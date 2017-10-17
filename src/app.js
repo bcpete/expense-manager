51520d256418ffeaaa07fd345c2ac72078b42ef8
hometownzero <bradycpeters@gmail.com>
2017-10-17 16:28:13 -0500

    refactored to one component per file, moved routes to router folder.

--- src/app.js
+++ src/app.js
@@ -1,62 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import AppRouter from './routers/AppRouter'
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from the dashboard component.
-  </div>
-);
-
-const AddExpensePage = () => (
-  <div>
-    This is from the add expense component.
-  </div>
-);
-
-const EditExpensePage = () => (
-  <div>
-    This is from the edit component.
-  </div>
-);
-
-const ExpenseHelpPage = () => (
-  <div>
-    This is from the help component.
-  </div>
-);
-
-const NotFoundPage = () => (
-  <div>
-    404! - <Link to="/">Go Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expense Manager</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>    
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={ExpenseHelpPage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
