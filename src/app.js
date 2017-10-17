1c079d33753bfe3b86da746985983ce917355804
hometownzero <bradycpeters@gmail.com>
2017-10-16 19:31:58 -0500

    added 404 for unregistered routes, added switch from react router.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,14 +28,21 @@ const ExpenseHelpPage = () => (
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={ExpenseHelpPage} />
-    </div>
+      <Route path="" component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
