f1d35228ca3fd1b5b93b0a45aad101a84849d4f3
hometownzero <bradycpeters@gmail.com>
2017-10-17 16:50:40 -0500

    changed edit page to now take an id afterwards and removed /edit from navlinks.

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    This is from the edit component.
-  </div>
-);
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      Editing expense number {props.match.params.id}
+    </div>
+  );
+};
 
 export default EditExpensePage;
