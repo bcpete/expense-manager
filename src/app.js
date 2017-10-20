1273ad64532e14a039b781b0e8ff71906090bc45
hometownzero <bradycpeters@gmail.com>
2017-10-19 20:31:28 -0500

    added expense list component, installed react-redux and mapped state to the component.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter'
@@ -9,14 +10,22 @@ import getVisibleExpenses from './selectors/visibleExpenses';
 import { setTextFilter } from './actions/filters';
 
 const store = configureStore();
+const state = store.getState();
+
 const expenseOne = store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 200}));
 const expenseTwo = store.dispatch(addExpense({ description: 'Gas bill', amount: 500, createdAt: 125}));
 store.dispatch(setTextFilter('bill'));
 
-const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses);
 
+console.log(visibleExpenses);
 console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+
+ReactDOM.render(jsx, document.getElementById('app'));
