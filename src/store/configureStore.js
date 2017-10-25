e6d172f9adbd572b79c606e2af492f6a988a0a3c
hometownzero <bradycpeters@gmail.com>
2017-10-25 17:18:55 -0500

    added redux devtools for chrome, finished the edit expense page, moved remove button from dashboard to edit expense page.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,15 @@
 import { createStore, combineReducers } from 'redux';
 import filtersReducer from './../reducers/filterReducer';
 import expensesReducer from './../reducers/expenseReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    composeWithDevTools()
   );
   return store;
 };
