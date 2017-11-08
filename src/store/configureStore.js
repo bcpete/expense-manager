76d20803f702fade066bd30ed4ed662dd18dfa7b
hometownzero <bradycpeters@gmail.com>
2017-11-08 17:21:38 -0600

    removed redux devtools import hopefully this fixes the issue.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,13 @@
 import { createStore, combineReducers } from 'redux';
 import filtersReducer from './../reducers/filterReducer';
 import expensesReducer from './../reducers/expenseReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    }),
-    composeWithDevTools()
+    })
   );
   return store;
 };
