e4cb61174d0a5e203c75a0b8ea1623c1f8f31665
hometownzero <bradycpeters@gmail.com>
2017-11-11 16:12:07 -0600

    added new selector to total expenses, and added test cases.

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListItem } from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
+import getExpensesTotal from '../../selectors/expenses-total';
 
 test('Should render expense list item', () => {
   const wrapper = shallow(<ExpenseListItem {...expenses[0]}/>);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('Should return 0 if no expenses', () => {
+  const totalExpenses = getExpensesTotal([]);
+  expect(totalExpenses).toBe(0);
+});
+
+test('Should correctly add up a single expense', () => {
+  const totalExpenses = getExpensesTotal([expenses[0]]);
+  expect(totalExpenses).toBe(expenses[0].amount);
+});
+
+test('Should correctly add up multiple expenses', () => {
+  const totalExpenses = getExpensesTotal(expenses);
+  expect(totalExpenses).toBe(114195);
 });
